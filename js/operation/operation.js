0564735e1766ff31393634142a6ddb83df9b7fa4
LTT <2650309004@qq.com>
2019-03-27 11:43:48 +0800

    修改 后台界面样式及逻辑变更

--- js/operation/operation.js
+++ js/operation/operation.js
@@ -1,15 +1,15 @@
 $(function(){
 	// 点击事件
-    $('.navbar').on('click','.btn',function(){
+    $('.layui-side-scroll').on('click','.btn',function(){		//左侧导航栏
         $(this).addClass('click_btn').parents('li').siblings().find('.btn').removeClass('click_btn');
     });
-    $('.navbar').on('click','.logout',function(){
+    $('.logout').on('click',function(){			//退出登录
     	$('#myalert').show();
     });
-    $('#alertConfirm').on('click','.right',function(){
+    $('#alertConfirm').on('click','.right',function(){			//退出登录弹框取消按钮
     	$('#myalert').hide();
     });
-    $('#alertConfirm').on('click','.left',function(){
+    $('#alertConfirm').on('click','.left',function(){			//退出登录弹框确认按钮
     	$('#myalert').hide();
         $.getJSON(global_path + "/logout", function(data) {
         	if(data.code == 0){
