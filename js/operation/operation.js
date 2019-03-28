e7069515559035f7970b6d29a3a9417efcaa733a
LTT <2650309004@qq.com>
2019-03-28 09:56:00 +0800

    修改 用户管理添加表格逻辑

--- js/operation/operation.js
+++ js/operation/operation.js
@@ -115,27 +115,75 @@ $(function(){
     
     // 用户部分
     function user(){
-    	layui.use('table', function(){
-	  		var table = layui.table;
+		layui.use('table', function(){
+	  		table = layui.table //表格
 			  
-			  //第一个实例
+		  	//执行一个 table 实例
 		  	table.render({
-			    elem: '#user'
-			    ,height: 312
+			    elem: '#demo'
 			    ,url: '../js/data.js' //数据接口
+			    ,title: '用户表'
 			    ,page: true //开启分页
+			    ,toolbar: 'default' //开启工具栏，此处显示默认图标，可以自定义模板，详见文档
+			    ,totalRow: true //开启合计行
 			    ,cols: [[ //表头
-			      {field: 'id', title: 'ID', width:80, sort: true, fixed: 'left'}
-			      ,{field: 'username', title: '用户名', width:80}
-			      ,{field: 'sex', title: '性别', width:80, sort: true}
-			      ,{field: 'city', title: '城市', width:80} 
-			      ,{field: 'sign', title: '签名', width: 177}
-			      ,{field: 'experience', title: '积分', width: 80, sort: true}
-			      ,{field: 'score', title: '评分', width: 80, sort: true}
-			      ,{field: 'classify', title: '职业', width: 80}
-			      ,{field: 'wealth', title: '财富', width: 135, sort: true}
+			      {type: 'checkbox', fixed: 'left'}
+			      ,{field: 'id', title: 'ID', width:80, sort: true, fixed: 'left', totalRowText: '合计：'}
+			      ,{field: 'username', title: '用户名', width:80, align:'center'}
+			      ,{field: 'experience', title: '积分', width: 90, sort: true, totalRow: true, align:'center'}
+			      ,{field: 'sex', title: '性别', width:100, sort: true, align:'center'}
+			      ,{field: 'score', title: '评分', width: 80, sort: true, totalRow: true, align:'center'}
+			      ,{field: 'city', title: '城市', width:150, align:'center'} 
+			      ,{field: 'sign', title: '签名', width: 200, align:'center'}
+			      ,{field: 'classify', title: '职业', width: 100, align:'center'}
+			      ,{field: 'wealth', title: '财富', width: 135, sort: true, totalRow: true, align:'center'}
+			      ,{fixed: 'right', width: 165, align:'center', toolbar: '#barDemo'}
 			    ]]
-			});
+		  	});
+			  
+		  	//监听头工具栏事件
+		  	table.on('toolbar(test)', function(obj){
+			    var checkStatus = table.checkStatus(obj.config.id)
+			    ,data = checkStatus.data; //获取选中的数据
+			    switch(obj.event){
+		      		case 'add':
+		        		layer.msg('添加');
+		      		break;
+	      			case 'update':
+			        	if(data.length === 0){
+			          		layer.msg('请选择一行');
+			        	} else if(data.length > 1){
+			          		layer.msg('只能同时编辑一个');
+	        			} else {
+			          		layer.alert('编辑 [id]：'+ checkStatus.data[0].id);
+			        	}
+			      	break;
+			      	case 'delete':
+			       	 	if(data.length === 0){
+			          		layer.msg('请选择一行');
+			        	} else {
+			          		layer.msg('删除');
+			        	}
+			      	break;
+			    };
+		  	});
+			  
+		  	//监听行工具事件
+		  	table.on('tool(test)', function(obj){ //注：tool 是工具条事件名，test 是 table 原始容器的属性 lay-filter="对应的值"
+			    var data = obj.data //获得当前行数据
+			    ,layEvent = obj.event; //获得 lay-event 对应的值
+			    if(layEvent === 'detail'){
+			      	layer.msg('查看操作');
+			    } else if(layEvent === 'del'){
+			      	layer.confirm('真的删除行么', function(index){
+			        	obj.del(); //删除对应行（tr）的DOM结构
+			        	layer.close(index);
+			        	//向服务端发送删除指令
+			      	});
+			    } else if(layEvent === 'edit'){
+			      	layer.msg('编辑操作');
+			    }
+		  	});
 			  
 		});
     }
