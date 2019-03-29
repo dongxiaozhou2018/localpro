d9e2fabfc70e771cea2a3bc26cf7b80768b0bbcf
LTT <2650309004@qq.com>
2019-03-29 11:13:59 +0800

    修改 添加信息逻辑

--- js/operation/add.js
+++ js/operation/add.js
@@ -1,4 +1,5 @@
 (function(){
+    var src;
     layui.use(['form', 'layedit', 'laydate',"jquery", "upload", "layer", "element"], function(){
         var $ = layui.$,
             form = layui.form,
@@ -14,38 +15,38 @@
             ,auto: false
             ,accept:'images'
             ,exts:'jpg|png|gif|bmp|jpeg'
-            // ,bindAction: '#test8'
+            ,bindAction: '#submit_btn'
+            ,before: function(obj){
+                this.data={
+                    'username':$('.username').val(),
+                    'password':$('.password').val(),
+                    'role':$('.role').val(),
+                    'remarks':$('.remarks').val(),
+                    'dept':$('.dept').val(),
+                    'telephone':$('.telephone').val()
+                };//关键代码
+            } 
             ,choose: function(obj){
                 var files = obj.pushFile();
+                
                 obj.preview(function(index, file, result){
+                    console.log($('.username').val())
                   $("#imgContent").attr('src',result);
                 });
             }
-        });
-      //监听提交
-      form.on('submit(demo1)', function(data){
-        console.log(data.field);
-        var parms = {
-            'username':data.field.username,
-            'password':data.field.password,
-            'role':data.field.role,
-            'remarks':data.field.remarks,
-            'dept':data.field.dept,
-            'telephone':data.field.telephone,
-            'file':data.field.file
-        }
-        var url = global_path + "/addUser";
-        commonAjax(url,parms,function(data){
-            if(data.code == 0){
-                $('#myalert').show();
-            }else{
-                alert(data.msg);
+            ,done: function(res, index, upload){
+                //假设code=0代表上传成功
+                if(res.code == 0){
+                    $('#myalert').show();
+                }else{
+                    alert(data.msg);
+                }
+                
             }
-        },function(){
-            alert('请求失败');
-        })
-        
-      });
+            ,error: function(index, upload){
+                // alert('请求失败');
+            }
+        });
       
     });
     
