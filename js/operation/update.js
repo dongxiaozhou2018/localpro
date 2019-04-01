590e09b20655122e8237a15a76e3d975315b0b16
LTT <2650309004@qq.com>
2019-04-01 16:10:17 +0800

    修改 添加、编辑用户信息新增是否输入判断逻辑

--- js/operation/update.js
+++ js/operation/update.js
@@ -20,8 +20,8 @@
             done: function (res) {
                 //如果上传失败
                 if (res.code == 0) {
+                    sessionStorage.setItem('fileId',res.data);
                     return layer.msg('上传成功');
-                    sessionStorage.setItem('fileId',res.fileId);
                 }else{
                     return layer.msg('上传失败');
                 }
@@ -49,28 +49,54 @@
         })
         form.on('submit(demo1)', function (data) {
             var fileId = sessionStorage.getItem('fileId');
-            var parms = {
-                'username':data.field.username,
-                'role':data.field.role,
-                'remarks':data.field.remarks,
-                'dept':data.field.dept,
-                'telephone':data.field.telephone,
-                'id':getQueryString('userID'),
-                'fileId':fileId ? fileId : ''
-            }
-            var url = global_path + "/updateUser";
-            commonAjax(url,parms,function(data){
-                if(data.code == 0){
-                    $('#myalert').show();
-                    $('#alertConfirm').on('click','a',function(){          //保存成功弹框取消按钮
-                        localStorage.removeItem('checkUser')
-                        $('#myalert').hide();
-                        window.location.href = "./operation.html";
-                    });
-                }else{
-                    alert(data.msg);
+            var username = $('.username').val();
+            var role = $('.role').val();
+            var dept = $('.dept').val();
+            var telephone = $('.telephone').val();
+            var remarks = $('.remarks').val();
+            if(!fileId || fileId == ''){
+                alert('请选择图片');
+                return;
+            }else if(username == ''){
+                alert('请输入用户名');
+                return;
+            }else if(role == ''){
+                alert('请选择用户类型');
+                return;
+            }else if(dept == ''){
+                alert('请输入用户部门');
+                return;
+            }else if(telephone == ''){
+                alert('请输入用户电话');
+                return;
+            }else if(remarks == ''){
+                alert('请输入用户备注信息');
+                return;
+            }else{
+                var parms = {
+                    'username':data.field.username,
+                    'role':data.field.role,
+                    'remarks':data.field.remarks,
+                    'dept':data.field.dept,
+                    'telephone':data.field.telephone,
+                    'id':getQueryString('userID'),
+                    'fileId':fileId ? fileId : ''
                 }
-            })
+                var url = global_path + "/updateUser";
+                commonAjax(url,parms,function(data){
+                    if(data.code == 0){
+                        $('#myalert').show();
+                        $('#alertConfirm').on('click','a',function(){          //保存成功弹框取消按钮
+                            localStorage.removeItem('checkUser')
+                            $('#myalert').hide();
+                            window.location.href = "./operation.html";
+                        });
+                    }else{
+                        alert(data.msg);
+                    }
+                })
+            }
+            
 
         });
     });
