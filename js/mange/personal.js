60cd834cd1fff7fcc68ffd26a431725d7563aa64
LTT <2650309004@qq.com>
2019-04-15 17:46:55 +0800

    修改 个人信息添加上传头像逻辑

--- js/mange/personal.js
+++ js/mange/personal.js
@@ -8,6 +8,48 @@
             ,upload = layui.upload
             ,element = layui.element;
 
+
+        var uploadInst = upload.render({
+            elem: '#test1',
+            url: global_path + "/manage/user/fileUpLoad",
+            headers: {
+                'at': at
+            },
+            before: function (obj) {
+                //预读本地文件示例，不支持ie8
+                obj.preview(function (index, file, result) {
+                    $('#demo1').attr('src', result); //图片链接（base64）
+                });
+                var file = sessionStorage.getItem('file');
+                if(file){
+                    file = JSON.parse(file).data;
+                    this.data = {
+                        'filename':file.filename
+                    }
+                }
+                
+            },
+            done: function (res) {
+                //如果上传失败
+                if (res.code == 0) {
+                    sessionStorage.setItem('file',JSON.stringify(res));
+                    return layer.msg('上传成功');
+                }else if(res.code == 401){
+                    unauthorized(res.code);
+                }else{
+                    return layer.msg('上传失败');
+                }
+                //上传成功
+            },
+            error: function () {
+                //演示失败状态，并实现重传
+                var demoText = $('#demoText');
+                demoText.html('<span style="color: #FF5722;">上传失败</span> <a class="layui-btn layui-btn-xs demo-reload">重试</a>');
+                demoText.find('.demo-reload').on('click', function () {
+                    uploadInst.upload();
+                });
+            }
+        });
         //表单初始赋值
         var HTlogin = sessionStorage.getItem('HTlogin');
         if(HTlogin){
@@ -20,6 +62,7 @@
             })
         }
         form.on('submit(demo1)', function (data) {
+            var file = sessionStorage.getItem('file');
             var realName = $('.realName').val();
             var username = $('.username').val();
             var dept = $('.dept').val();
@@ -43,6 +86,13 @@
                     'dept':data.field.dept,
                     'telephone':data.field.telephone,
                     'id':HTlogin.data.id
+                }
+                if(file){
+                    file = JSON.parse(file).data;
+                    parms.filename = file.filename;
+                    parms.filetype = file.filetype;
+                    parms.suffix = file.suffix;
+                    parms.url = file.url;
                 }
                 var url = global_path + "/manage/user/editPersonalInfo";
                 commonAjax(url,parms,function(data){
